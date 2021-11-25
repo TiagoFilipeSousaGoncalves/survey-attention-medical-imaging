cbd42cb46af7ef9ca884d678187c91c99c61b7ca
Tiago Gonçalves <tiago.galves@hotmail.com>
2021-11-25 11:57:42 +0000

    Added new stuff

--- code/cbis_train.py
+++ code/cbis_train.py
@@ -89,7 +89,7 @@ EPOCHS = 300
 LOSS = torch.nn.CrossEntropyLoss()
 LEARNING_RATE = 1e-4
 OPTIMISER = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
-BATCH_SIZE = 6
+BATCH_SIZE = 4
 
 
 # Load data
