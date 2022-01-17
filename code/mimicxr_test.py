e5e7aa09922d215455cf6d7003825263f2b2da2f
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-01-17 10:39:02 +0000

    [MIMICXR] - Added CBAMVGG16 to training

--- code/mimicxr_test.py
+++ code/mimicxr_test.py
@@ -86,12 +86,12 @@ _, _, nr_classes = map_images_and_labels(base_data_path=test_dir, pickle_path=os
 
 # CBAM Models
 # CBAMResNet50
-model = CBAMResNet50(channels=img_nr_channels, height=img_height, width=img_width, nr_classes=nr_classes)
-model_name = "cbamresnet50"
+# model = CBAMResNet50(channels=img_nr_channels, height=img_height, width=img_width, nr_classes=nr_classes)
+# model_name = "cbamresnet50"
 
 # CBAMVGG16
-# model = CBAMVGG16(channels=img_nr_channels, height=img_height, width=img_width, nr_classes=nr_classes)
-# model_name = "cbamvgg16"
+model = CBAMVGG16(channels=img_nr_channels, height=img_height, width=img_width, nr_classes=nr_classes)
+model_name = "cbamvgg16"
 
 # CBAMDenseNet121
 # model = CBAMDenseNet121(channels=img_nr_channels, height=img_height, width=img_width, nr_classes=nr_classes)
