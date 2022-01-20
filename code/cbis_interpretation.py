7de6d1f9f452665247f403b705f8b74391a9ed7b
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-01-20 16:10:00 +0000

    [CBIS] - Added a small debug test to model.load_state_dict

--- code/cbis_interpretation.py
+++ code/cbis_interpretation.py
@@ -140,6 +140,7 @@ BATCH_SIZE = 1
 # Load model weights
 missing, unexpected = model.load_state_dict(torch.load(os.path.join(weights_dir, f"{model_name}_cbis.pt"), map_location=DEVICE), strict=False)
 print(len(missing), len(unexpected))
+print(missing)
 model.eval()
 
 
