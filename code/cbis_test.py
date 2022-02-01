18bd6b2b9e46a1bf614bf6edff8e2b441246e224
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-02-01 14:47:09 +0000

    Minor features added

--- code/cbis_test.py
+++ code/cbis_test.py
@@ -243,7 +243,7 @@ with torch.no_grad():
     # val_f1 = f1_score(y_true=y_val_true, y_pred=y_val_pred)
 
     # Print Statistics
-    print(f"Model Name: {model_name}\t{split} Loss: {avg_test_loss}\t{data_split} Accuracy: {test_acc}")
+    print(f"Model Name: {model_name}\t{data_split} Loss: {avg_test_loss}\t{data_split} Accuracy: {test_acc}")
     # print(f"Validation Loss: {avg_val_loss}\tValidation Accuracy: {val_acc}\tValidation Recall: {val_recall}\tValidation Precision: {val_precision}\tValidation F1-Score: {val_f1}")
 
 
