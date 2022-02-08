69ddd887a2e093524762a85a94b4cb4c6aaab693
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-02-08 10:29:00 +0000

    [MIMIC-CXR] Added new script for SLURM

--- train_mimicxr.sh
+++ train_mimicxr.sh
@@ -2,4 +2,4 @@
 #SBATCH --gres=gpu:1
 #SBATCH -o fich-mimicxr-%j.out
 #SBATCH -e fich-mimicxr-%j.err
-python code/models_train.py --dataset MIMICXR --model SEVGG16 --batchsize 16
+python code/models_train.py --dataset MIMICXR --model SEVGG16 --batchsize 4
