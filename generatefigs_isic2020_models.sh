4dc98ba24fc785cf6ced87d7ddeb37f1350c816d
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-03-04 15:39:11 +0000

    Added new stuff

---
+++ generatefigs_isic2020_models.sh
@@ -0,0 +1,6 @@
+python code/generate_xai_figures.py --modelckpt results/isic2020/densenet121/2022-03-01_18-19-07
+python code/generate_xai_figures.py --modelckpt results/isic2020/sedensenet121/2022-03-02_01-12-08
+python code/generate_xai_figures.py --modelckpt results/isic2020/cbamdensenet121/2022-03-02_08-19-52
+python code/generate_xai_figures.py --modelckpt results/isic2020/resnet50/2022-02-28_22-58-32
+python code/generate_xai_figures.py --modelckpt results/isic2020/seresnet50/2022-03-01_04-29-33
+python code/generate_xai_figures.py --modelckpt results/isic2020/cbamresnet50/2022-03-01_10-57-05
