4e6ab98a52c561740c02f677854b17718b615cb4
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-03-06 21:52:33 +0000

    [MIMICCXR] Tests and saliency maps on the way

---
+++ test_mimicxr_resnet.sh
@@ -0,0 +1,3 @@
+python code/models_test.py --dataset MIMICCXR --split Train --model ResNet50 --batchsize 32 --modelckpt results/mimiccxr/resnet50/2022-03-02_14-53-12/
+python code/models_test.py --dataset MIMICCXR --split Validation --model ResNet50 --batchsize 32 --modelckpt results/mimiccxr/resnet50/2022-03-02_14-53-12/
+python code/models_test.py --dataset MIMICCXR --split Test --model ResNet50 --batchsize 32 --modelckpt results/mimiccxr/resnet50/2022-03-02_14-53-12/
--- train_mimicxr_resnet.sh
+++
@@ -1,3 +0,0 @@
-python code/models_train.py --dataset MIMICCXR --model ResNet50 --batchsize 32 --classweights --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 50
-python code/models_train.py --dataset MIMICCXR --model SEResNet50 --batchsize 32 --classweights --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 50
-python code/models_train.py --dataset MIMICCXR --model CBAMResNet50 --batchsize 32 --classweights --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 50
