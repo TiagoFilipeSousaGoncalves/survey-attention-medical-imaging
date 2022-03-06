b9c0d5637cf885f1f28f407ee35a0c7310513482
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-03-06 22:36:07 +0000

    [MIMICCXR] Tests and saliency maps on the way

--- test_mimicxr_cbamresnet.sh
+++
@@ -1,3 +0,0 @@
-python code/models_test.py --dataset MIMICCXR --split Train --model CBAMResNet50 --batchsize 32 --modelckpt results/mimiccxr/cbamresnet50/2022-03-04_13-44-38
-python code/models_test.py --dataset MIMICCXR --split Validation --model CBAMResNet50 --batchsize 32 --modelckpt results/mimiccxr/cbamresnet50/2022-03-04_13-44-38
-python code/models_test.py --dataset MIMICCXR --split Test --model CBAMResNet50 --batchsize 32 --modelckpt results/mimiccxr/cbamresnet50/2022-03-04_13-44-38
