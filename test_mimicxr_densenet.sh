e02b67e3d13c3a0b10fd36dbd97be080b375d979
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-03-06 23:02:53 +0000

    [MIMICCXR] Tests and saliency maps on the way

--- test_mimicxr_densenet.sh
+++
@@ -1,3 +0,0 @@
-python code/models_test.py --dataset MIMICCXR --split Train --model DenseNet121 --batchsize 32 --modelckpt results/mimiccxr/densenet121/2022-03-02_14-52-30
-python code/models_test.py --dataset MIMICCXR --split Validation --model DenseNet121 --batchsize 32 --modelckpt results/mimiccxr/densenet121/2022-03-02_14-52-30
-python code/models_test.py --dataset MIMICCXR --split Test --model DenseNet121 --batchsize 32 --modelckpt results/mimiccxr/densenet121/2022-03-02_14-52-30
