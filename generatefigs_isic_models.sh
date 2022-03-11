edfc1549ecc4ca8302a6ebd29ae6376e60e5aa1e
Tiago Filipe Sousa Gonçalves <tiago.f.goncalves@inesctec.pt>
2022-03-11 11:11:18 +0000

    [DATASETS] Low data regimen added to ISIC2020 and MIMICCXR

---
+++ generatefigs_isic_models.sh
@@ -0,0 +1,6 @@
+python code/generate_xai_figures.py --modelckpt results/isic2020/densenet121/2022-03-01_18-19-07
+python code/generate_xai_figures.py --modelckpt results/isic2020/sedensenet121/2022-03-02_01-12-08
+python code/generate_xai_figures.py --modelckpt results/isic2020/cbamdensenet121/2022-03-02_08-19-52
+python code/generate_xai_figures.py --modelckpt results/isic2020/resnet50/2022-03-04_12-11-36
+python code/generate_xai_figures.py --modelckpt results/isic2020/seresnet50/2022-03-04_20-18-10
+python code/generate_xai_figures.py --modelckpt results/isic2020/cbamresnet50/2022-03-05_05-26-22
---
+++ interpretation_aptos.sh
@@ -0,0 +1,6 @@
+python code/models_interpretation.py --dataset APTOS --split Test --model DenseNet121 --modelckpt results/aptos/densenet121/2022-03-02_20-18-23 --batchsize 1
+python code/models_interpretation.py --dataset APTOS --split Test --model SEDenseNet121 --modelckpt results/aptos/sedensenet121/2022-03-02_21-16-28 --batchsize 1
+python code/models_interpretation.py --dataset APTOS --split Test --model CBAMDenseNet121 --modelckpt results/aptos/cbamdensenet121/2022-03-02_22-17-26 --batchsize 1
+python code/models_interpretation.py --dataset APTOS --split Test --model ResNet50 --modelckpt results/aptos/resnet50/2022-03-02_17-57-24 --batchsize 1
+python code/models_interpretation.py --dataset APTOS --split Test --model SEResNet50 --modelckpt results/aptos/seresnet50/2022-03-02_18-46-07 --batchsize 1
+python code/models_interpretation.py --dataset APTOS --split Test --model CBAMResNet50 --modelckpt results/aptos/cbamresnet50/2022-03-02_19-41-49 --batchsize 1
