1d3082aa05d010530c1bd0dd11cd483eb2b443b0
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-03-17 10:19:23 +0000

    [ISIC2020] Added xAI maps and added new figures

--- generatefigs_isic_models.sh
+++ generatefigs_isic_models.sh
@@ -1,6 +1,6 @@
-python code/generate_xai_figures.py --modelckpt results/isic2020/densenet121/2022-03-01_18-19-07 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/isic2020/sedensenet121/2022-03-02_01-12-08 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/isic2020/cbamdensenet121/2022-03-02_08-19-52 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/isic2020/densenet121/2022-03-01_18-19-07 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/isic2020/sedensenet121/2022-03-02_01-12-08 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/isic2020/cbamdensenet121/2022-03-02_08-19-52 --alpha_overlay 0.6
 python code/generate_xai_figures.py --modelckpt results/isic2020/resnet50/2022-03-04_12-11-36 --alpha_overlay 0.6
 python code/generate_xai_figures.py --modelckpt results/isic2020/seresnet50/2022-03-04_20-18-10--alpha_overlay 0.6
 python code/generate_xai_figures.py --modelckpt results/isic2020/cbamresnet50/2022-03-05_05-26-22 --alpha_overlay 0.6
