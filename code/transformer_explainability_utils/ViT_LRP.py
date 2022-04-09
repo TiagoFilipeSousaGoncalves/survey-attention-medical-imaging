70c7d29763e63eb8d6a31fafbbb72bb355a73008
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-09 16:53:10 +0100

    [DeiT] Added model compatible with Transformers-LRP

--- code/transformer_explainability_utils/ViT_LRP.py
+++ code/transformer_explainability_utils/ViT_LRP.py
@@ -7,10 +7,10 @@ import torch.nn as nn
 from einops import rearrange
 
 # Custom Imports
-from layers_ours import *
-from helpers import load_pretrained
-from weight_init import trunc_normal_
-from layer_helpers import to_2tuple
+from .layers_ours import *
+from .helpers import load_pretrained
+from .weight_init import trunc_normal_
+from .layer_helpers import to_2tuple
 
 
 
