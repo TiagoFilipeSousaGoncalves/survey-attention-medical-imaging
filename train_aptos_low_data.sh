de607a540d4100d886e46b11aa5e554dc72c32d2
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-10 00:41:25 +0100

    Added DeiT-LRP to other datasets

--- train_aptos_low_data.sh
+++ train_aptos_low_data.sh
@@ -3,6 +3,8 @@
 #SBATCH -o job-%j.out
 #SBATCH -e job-%j.err
 
+
+
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model DenseNet121 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model SEDenseNet121 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model CBAMDenseNet121 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
