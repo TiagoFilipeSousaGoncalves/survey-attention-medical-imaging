68a6830a5d5d8ce8e87b400541696fcc51de20bc
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-09 19:01:17 +0100

    [APTOS2019] - Added DeiT-LRP for training mode

--- train_aptos_low_data.sh
+++ train_aptos_low_data.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --gres=gpu:1
-# other options you might want
-# etc
+#SBATCH -o job-%j.out
+#SBATCH -e job-%j.err
+
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model DenseNet121 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model SEDenseNet121 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model CBAMDenseNet121 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
@@ -10,4 +11,3 @@
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model CBAMResNet50 --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model DeiT --low_data_regimen --perc_train 0.5 --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
 python code/models_train.py --data_dir /ctm-hdd-pool01/tgoncalv/datasets/APTOS2019_resized/ --dataset APTOS --model DeiT-LRP --batchsize 32 --epochs 300 --lr 1e-6 --num_workers 2 --gpu_id 0 --save_freq 1000
-
