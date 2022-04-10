fb467b00cb0c75a6f54211b52a6b6bfdf899c894
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-10 23:58:16 +0100

    [APTOS] Some bug fixes.

--- code/generate_xai_figures.py
+++ code/generate_xai_figures.py
@@ -28,10 +28,10 @@ parser = argparse.ArgumentParser()
 
 # Add the arguments
 # Model checkpoint
-parser.add_argument("--modelckpt", type=str, required=True, help="Directory where model is stored")
+parser.add_argument("--modelckpt", type=str, required=True, help="Directory where model is stored.")
 
 # Type of saliency maps
-parser.add_argument("--saliency_maps", type=str, required=True, choices=["ALL", "DEEPLIFT", "LRP"], help="Saliency maps: ALL, DEEPLIFT, LRP")
+parser.add_argument("--saliency_maps", type=str, required=True, choices=["ALL", "DEEPLIFT", "LRP"], help="Saliency maps: ALL, DEEPLIFT, LRP.")
 
 # Alpha overlay for saliency maps
 parser.add_argument("--alpha_overlay", type=float, default=0.5, help="Alpha parameter for overlayed saliency maps.")
@@ -46,13 +46,13 @@ modelckpt = args.modelckpt
 
 # Saliency maps
 saliency_maps = args.saliency_maps
-print(saliency_maps)
+# print(saliency_maps)
 
 # Alpha overlay
 alpha_overlay = args.alpha_overlay
 
 
-    
+
 # Set the directory of the xAI maps
 xai_maps_dir = os.path.join(modelckpt, "xai_maps")
 
@@ -66,21 +66,14 @@ if not(os.path.isdir(png_figs_dir)):
 
 
 # Create a list of sub-directories
-sub_dirs = ["original-imgs", "deeplift", "lrp"]
+if saliency_maps == "ALL":
+    sub_dirs = ["original-imgs", "deeplift", "lrp"]
 
+elif saliency_maps == "DEEPLIFT":
+    sub_dirs = ["original-imgs", "deeplift"]
 
-
-# Get the files
-attribute_flist = os.listdir(os.path.join(xai_maps_dir, sub_dirs[0]))
-attribute_flist = [i for i in attribute_flist if not i.startswith('.')]
-attribute_flist.sort()
-
-
-# .PNG sub-dirs
-for sub_dir_name in sub_dirs:
-    png_sub_dir = os.path.join(png_figs_dir, sub_dir_name)
-    if not(os.path.isdir(png_sub_dir)):
-        os.makedirs(png_sub_dir)
+elif saliency_maps == "LRP":
+    sub_dirs = ["original-imgs", "lrp"]
 
 
 
@@ -88,78 +81,90 @@ for sub_dir_name in sub_dirs:
 print(f"Creating figures from: {modelckpt}")
 
 # Loop through files
-for fname in attribute_flist:
+# First, we create the .PNG sub-dirs
+for sub_dir_name in sub_dirs:
+    png_sub_dir = os.path.join(png_figs_dir, sub_dir_name)
+    if not(os.path.isdir(png_sub_dir)):
+        os.makedirs(png_sub_dir)
     
 
-    # Try to generate the final images of the attributes
-    try:
+    # Then, we get the files
+    attribute_flist = os.listdir(os.path.join(xai_maps_dir, sub_dir_name))
+    attribute_flist = [i for i in attribute_flist if not i.startswith('.')]
+    attribute_flist.sort()
 
-        # Original Image
-        original_fname = os.path.join(xai_maps_dir, sub_dirs[0], fname)
-        original_img = np.load(original_fname, allow_pickle=True)
-
-        # Get figure
-        figure, axis = viz.visualize_image_attr(None, original_img, method="original_image", use_pyplot=False)
-
-        # Get the figure from memory
-        convert_figure(figure)
+    for fname in attribute_flist:
         
-        # Save figure
-        plt.axis('off')
-        plt.savefig(os.path.join(png_figs_dir, sub_dirs[0], fname.split('.')[0]+'.png'), bbox_inches='tight')
-        plt.clf()
-        # plt.show()
-        plt.close()
 
+        # Try to generate the final images of the attributes
+        try:
 
-
-        # Deeplift
-        if saliency_maps in ("ALL", "DEEPLIFT"):
-            print("Generating DeepLIFT saliency maps images.")
-            deeplift_fname = os.path.join(xai_maps_dir, sub_dirs[1], fname)
-            deeplift_map = np.load(deeplift_fname, allow_pickle=True)
-            # print(deeplift_map.min(), deeplift_map.max(), deeplift_map.mean())
+            # Original Image
+            original_fname = os.path.join(xai_maps_dir, sub_dir_name, fname)
+            original_img = np.load(original_fname, allow_pickle=True)
 
             # Get figure
-            # figure, axis = viz.visualize_image_attr(deeplift_map, original_img, method="blended_heat_map", sign="all", show_colorbar=False, use_pyplot=False)
-            figure, axis = viz.visualize_image_attr(deeplift_map, original_img, method="blended_heat_map", sign="all", cmap='bwr', show_colorbar=False, use_pyplot=False, alpha_overlay=alpha_overlay)
-            
+            figure, axis = viz.visualize_image_attr(None, original_img, method="original_image", use_pyplot=False)
+
             # Get the figure from memory
             convert_figure(figure)
-
+            
             # Save figure
             plt.axis('off')
-            plt.savefig(os.path.join(png_figs_dir, sub_dirs[1], fname.split('.')[0]+'.png'), bbox_inches='tight')
+            plt.savefig(os.path.join(png_figs_dir, sub_dir_name, fname.split('.')[0]+'.png'), bbox_inches='tight')
             plt.clf()
             # plt.show()
             plt.close()
 
 
 
-        # LRP
-        elif saliency_maps in ("ALL", "LRP"):
-            print("Generating LRP saliency maps images.")
-            lrp_fname = os.path.join(xai_maps_dir, sub_dirs[2], fname)
-            lrp_map = np.load(lrp_fname, allow_pickle=True)
+            # Deeplift
+            if sub_dir_name == "deeplift":
+                print("Generating DeepLIFT saliency maps images.")
+                deeplift_fname = os.path.join(xai_maps_dir, sub_dir_name, fname)
+                deeplift_map = np.load(deeplift_fname, allow_pickle=True)
+                # print(deeplift_map.min(), deeplift_map.max(), deeplift_map.mean())
 
-            # Get figure
-            # figure, axis = viz.visualize_image_attr(lrp_map, original_img, method="blended_heat_map", sign="all", show_colorbar=False, use_pyplot=False)
-            figure, axis = viz.visualize_image_attr(lrp_map, original_img, method="blended_heat_map", sign="all", cmap='bwr', show_colorbar=False, use_pyplot=False, alpha_overlay=alpha_overlay)
-            
-            # Get the figure from memory
-            convert_figure(figure)
+                # Get figure
+                # figure, axis = viz.visualize_image_attr(deeplift_map, original_img, method="blended_heat_map", sign="all", show_colorbar=False, use_pyplot=False)
+                figure, axis = viz.visualize_image_attr(deeplift_map, original_img, method="blended_heat_map", sign="all", cmap='bwr', show_colorbar=False, use_pyplot=False, alpha_overlay=alpha_overlay)
+                
+                # Get the figure from memory
+                convert_figure(figure)
 
-            # Save figure
-            plt.axis('off')
-            plt.savefig(os.path.join(png_figs_dir, sub_dirs[2], fname.split('.')[0]+'.png'), bbox_inches='tight')
-            plt.clf()
-            # plt.show()
-            plt.close()
-    
+                # Save figure
+                plt.axis('off')
+                plt.savefig(os.path.join(png_figs_dir, sub_dir_name, fname.split('.')[0]+'.png'), bbox_inches='tight')
+                plt.clf()
+                # plt.show()
+                plt.close()
 
-    # Pass if the image + attribute has bad quality
-    except:
-        pass
+
+
+            # LRP
+            elif sub_dir_name == "lrp":
+                print("Generating LRP saliency maps images.")
+                lrp_fname = os.path.join(xai_maps_dir, sub_dir_name, fname)
+                lrp_map = np.load(lrp_fname, allow_pickle=True)
+
+                # Get figure
+                # figure, axis = viz.visualize_image_attr(lrp_map, original_img, method="blended_heat_map", sign="all", show_colorbar=False, use_pyplot=False)
+                figure, axis = viz.visualize_image_attr(lrp_map, original_img, method="blended_heat_map", sign="all", cmap='bwr', show_colorbar=False, use_pyplot=False, alpha_overlay=alpha_overlay)
+                
+                # Get the figure from memory
+                convert_figure(figure)
+
+                # Save figure
+                plt.axis('off')
+                plt.savefig(os.path.join(png_figs_dir, sub_dir_name, fname.split('.')[0]+'.png'), bbox_inches='tight')
+                plt.clf()
+                # plt.show()
+                plt.close()
+        
+
+        # Pass if the image + attribute has bad quality
+        except:
+            pass
 
 
 
