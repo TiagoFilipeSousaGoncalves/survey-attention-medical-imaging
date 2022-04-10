d7f3b85ca64206d848c68fc77d8ffe26e523c9e0
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-10 01:34:34 +0100

    Added DeiT-LRP to other datasets

--- train_mimiccxr_low_data.sh
+++ train_mimiccxr_low_data.sh
@@ -8,4 +8,4 @@
 # python code/models_train.py --data_dir /ctm-hdd-pool01/wjsilva19/MedIA --dataset MIMICCXR --model DeiT --low_data_regimen --perc_train 0.01 --batchsize 32 --classweights --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/wjsilva19/MedIA --dataset MIMICCXR --model DeiT --low_data_regimen --perc_train 0.1 --batchsize 32 --classweights --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 1000
 # python code/models_train.py --data_dir /ctm-hdd-pool01/wjsilva19/MedIA --dataset MIMICCXR --model DeiT --low_data_regimen --perc_train 0.5 --batchsize 32 --classweights --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 1000
-python code/models_train.py --data_dir /ctm-hdd-pool01/wjsilva19/MedIA --dataset MIMICCXR --model DeiT-LRP --batchsize 16 --classweights --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 1 --save_freq 1000
+python code/models_train.py --data_dir /ctm-hdd-pool01/wjsilva19/MedIA --dataset MIMICCXR --model DeiT-LRP --batchsize 16 --classweights --epochs 300 --lr 1e-6 --num_workers 4 --gpu_id 0 --save_freq 1000
