aadce19f0533f7c9cf09a1a9e854c8a6ce282cee
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-12 00:45:02 +0100

    Added new stuff

--- test_aptos_low_data.sh
+++ test_aptos_low_data.sh
@@ -1,8 +1,17 @@
-# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Train --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/aptos/deit/2022-03-30_23-09-48 --batchsize 32 --num_workers 4 --gpu_id 0
-python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Train --model DeiT-T-LRP --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --batchsize 32 --num_workers 4 --gpu_id 0
+# Train Split
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Train --model DeiT-T-LRP --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --batchsize 32 --num_workers 4 --gpu_id 0
+python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/aptos/deit-t-lrp/2022-04-11_14-48-27 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/aptos/deit-t-lrp/2022-04-11_15-46-15 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/aptos/deit-t-lrp/2022-04-11_17-01-22 --batchsize 32 --num_workers 4 --gpu_id 0
 
-# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Validation --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/aptos/deit/2022-03-30_23-09-48 --batchsize 32 --num_workers 4 --gpu_id 0
-python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Validation --model DeiT-T-LRP --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --batchsize 32 --num_workers 4 --gpu_id 0
+# Validation Split
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Validation --model DeiT-T-LRP --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --batchsize 32 --num_workers 4 --gpu_id 0
+python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/aptos/deit-t-lrp/2022-04-11_14-48-27 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/aptos/deit-t-lrp/2022-04-11_15-46-15 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/aptos/deit-t-lrp/2022-04-11_17-01-22 --batchsize 32 --num_workers 4 --gpu_id 0
 
-# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Test --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/aptos/deit/2022-03-30_23-09-48 --batchsize 32 --num_workers 4 --gpu_id 0
-python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Test --model DeiT-T-LRP --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --batchsize 32 --num_workers 4 --gpu_id 0
+# Test Split
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Test --model DeiT-T-LRP --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --batchsize 32 --num_workers 4 --gpu_id 0
+python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Test --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/aptos/deit-t-lrp/2022-04-11_14-48-27 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Test --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/aptos/deit-t-lrp/2022-04-11_15-46-15 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/APTOS2019/resized/APTOS2019_resized/ --dataset APTOS --split Test --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/aptos/deit-t-lrp/2022-04-11_17-01-22 --batchsize 32 --num_workers 4 --gpu_id 0
