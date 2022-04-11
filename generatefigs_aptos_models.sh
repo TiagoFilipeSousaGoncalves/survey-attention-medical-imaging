f3a053a1ea5d143395c342c135ebdaf720a1cdfa
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-11 23:04:54 +0100

    Added new stuff

--- generatefigs_aptos_models.sh
+++ generatefigs_aptos_models.sh
@@ -4,4 +4,4 @@
 # python code/generate_xai_figures.py --modelckpt results/aptos/resnet50/2022-03-02_17-57-24 --alpha_overlay 0.6
 # python code/generate_xai_figures.py --modelckpt results/aptos/seresnet50/2022-03-02_18-46-07 --alpha_overlay 0.6
 # python code/generate_xai_figures.py --modelckpt results/aptos/cbamresnet50/2022-03-02_19-41-49 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --saliency_maps LRP --alpha_overlay 0.6
+python code/generate_xai_figures.py --modelckpt results/aptos/deit-t-lrp/2022-04-11_12-29-28 --saliency_maps LRP --alpha_overlay 0.6
