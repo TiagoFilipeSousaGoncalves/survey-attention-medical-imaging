e435e9324e8beb5164cae32879cfb0cce29b6fcf
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-12 13:50:04 +0100

    [ISIC2020] Added new script for low data regimen.

--- test_isic_low_data.sh
+++ test_isic_low_data.sh
@@ -1,11 +1,17 @@
 # Train
-# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Train --model DeiT --low_data_regimen --perc_train 0.01 --modelckpt results/isic2020/deit/2022-03-28_10-28-49 --batchsize 32 --num_workers 4 --gpu_id 1
-python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Train --model DeiT-T-LRP  --modelckpt results/isic2020/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 1
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Train --model DeiT-T-LRP  --modelckpt results/isic2020/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 1
+python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_00-27-56 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_02-48-55 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_05-51-38 --batchsize 32 --num_workers 4 --gpu_id 0
 
 # Validation
-# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Validation --model DeiT --low_data_regimen --perc_train 0.01 --modelckpt results/isic2020/deit/2022-03-28_10-28-49 --batchsize 32 --num_workers 4 --gpu_id 1
-python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Validation --model DeiT-T-LRP  --modelckpt results/isic2020/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 1
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Validation --model DeiT-T-LRP  --modelckpt results/isic2020/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 1
+python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_00-27-56 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_02-48-55 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_05-51-38 --batchsize 32 --num_workers 4 --gpu_id 0
 
 # Test
-# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Test --model DeiT --low_data_regimen --perc_train 0.01 --modelckpt results/isic2020/deit/2022-03-28_10-28-49 --batchsize 32 --num_workers 4 --gpu_id 1
-python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Test --model DeiT-T-LRP  --modelckpt results/isic2020/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 1
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Test --model DeiT-T-LRP  --modelckpt results/isic2020/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 1
+python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Test --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_00-27-56 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Test --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_02-48-55 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/ISIC2020/ --dataset ISIC2020 --split Test --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/isic2020/deit-t-lrp/2022-04-12_05-51-38 --batchsize 32 --num_workers 4 --gpu_id 0
