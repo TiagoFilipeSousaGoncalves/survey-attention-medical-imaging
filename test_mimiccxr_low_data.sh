d2bc0a8beec8d0baf3bd339ea69d7cdfe7577e8a
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-12 13:45:01 +0100

    [ISIC2020] Added new script for low data regimen.

--- test_mimiccxr_low_data.sh
+++ test_mimiccxr_low_data.sh
@@ -1,17 +1,17 @@
 # Train
-python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/mimiccxr/deit/2022-03-28_18-06-59 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/mimiccxr/deit/2022-03-28_18-06-59 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/mimiccxr/deit/2022-03-28_18-06-59 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 0
+python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_00-27-56 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_02-48-55 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Train --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_05-51-38 --batchsize 32 --num_workers 4 --gpu_id 0
 
 # Validation
-python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/mimiccxr/deit/2022-03-28_18-25-13 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/mimiccxr/deit/2022-03-28_18-25-13 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/mimiccxr/deit/2022-03-28_18-25-13 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 0
+python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.01 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_00-27-56 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.1 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_02-48-55 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Validation --model DeiT-T-LRP --low_data_regimen --perc_train 0.5 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_05-51-38 --batchsize 32 --num_workers 4 --gpu_id 0
 
 # Test
-python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/mimiccxr/deit/2022-03-28_20-20-43 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/mimiccxr/deit/2022-03-28_20-20-43 --batchsize 32 --num_workers 4 --gpu_id 0
-# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/mimiccxr/deit/2022-03-28_20-20-43 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 32 --num_workers 4 --gpu_id 0
+python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT --low_data_regimen --perc_train 0.01 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_00-27-56 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT --low_data_regimen --perc_train 0.1 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_02-48-55 --batchsize 32 --num_workers 4 --gpu_id 0
+# python code/models_test.py --data_dir data/MedIA --dataset MIMICCXR --split Test --model DeiT --low_data_regimen --perc_train 0.5 --modelckpt results/mimiccxr/deit-t-lrp/2022-04-12_05-51-38 --batchsize 32 --num_workers 4 --gpu_id 0
