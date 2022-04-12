ff0ff4e4d8b440d0da15eb7f3850f707576ca050
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-12 10:40:01 +0100

    [MIMICCXR] Added test and interpretation scripts.

--- generatefigs_mimiccxr_models.sh
+++ generatefigs_mimiccxr_models.sh
@@ -1,6 +1,7 @@
-python code/generate_xai_figures.py --modelckpt results/mimiccxr/densenet121/2022-03-02_14-52-30 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/mimiccxr/sedensenet121/2022-03-04_10-27-00 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/mimiccxr/cbamdensenet121/2022-03-05_13-17-36 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/mimiccxr/resnet50/2022-03-02_14-53-12 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/mimiccxr/seresnet50/2022-03-04_09-03-01 --alpha_overlay 0.6
-python code/generate_xai_figures.py --modelckpt results/mimiccxr/cbamresnet50/2022-03-08_16-16-53 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/mimiccxr/densenet121/2022-03-02_14-52-30 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/mimiccxr/sedensenet121/2022-03-04_10-27-00 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/mimiccxr/cbamdensenet121/2022-03-05_13-17-36 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/mimiccxr/resnet50/2022-03-02_14-53-12 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/mimiccxr/seresnet50/2022-03-04_09-03-01 --alpha_overlay 0.6
+# python code/generate_xai_figures.py --modelckpt results/mimiccxr/cbamresnet50/2022-03-08_16-16-53 --alpha_overlay 0.6
+python code/generate_xai_figures.py --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --saliency_maps LRP --alpha_overlay 0.6
--- interpretation_mimicxr.sh
+++ interpretation_mimicxr.sh
@@ -1,1 +1,1 @@
-python code/models_interpretation.py --dataset MIMICCXR --split Test --model DeiT --modelckpt results/mimiccxr/deit/2022-03-02_23-28-00 --batchsize 1
+python code/models_interpretation.py --dataset MIMICCXR --split Test --model DeiT-T-LRP --modelckpt results/mimiccxr/deit-t-lrp/2022-04-11_12-31-47 --batchsize 1
