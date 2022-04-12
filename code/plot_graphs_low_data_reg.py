a9250c1c113a484fe836ce81e4224c63b9ca9145
Tiago Gonçalves <tiago.galves@hotmail.com>
2022-04-12 23:56:26 +0100

    [MIMICCXR] Added last low data regimen tests.

--- code/plot_graphs_low_data_reg.py
+++ code/plot_graphs_low_data_reg.py
@@ -1,12 +1,23 @@
+# Imports
 import pandas as pd
 import matplotlib.pyplot as plt
-xl = pd.ExcelFile("Experiments.xlsx")
+
+
+
+# Read Excel
+xl = pd.ExcelFile("results/experiments.xlsx")
+
+# Get datasets' names
 datasets = xl.sheet_names
+
+# Parameters for the plot
 x = [1, 2, 3, 4]
 x_labels = ["1%", "10%", "50%", "100%"]
 colors = ['C0', 'C1', 'C0', 'C1', 'C0', 'C1', 'C2']
 style = ['solid', 'solid', 'dashed', 'dashed', 'dotted', 'dotted', 'solid']
 markers = ['s', 'D', 's', 'D', 's', 'D', 'o']
+
+# Got through the Excel
 for d in datasets:
     print(d)
     df = xl.parse(d)
@@ -25,3 +36,4 @@ for d in datasets:
     # plt.show()
     plt.savefig(f'plot_{d}.png', bbox_inches='tight')
 
+print("Finished.")
